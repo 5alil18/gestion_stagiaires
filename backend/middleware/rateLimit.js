73fa0c05c6a91851f6916a5e91fd256eed140991
5alil18 <bouakrifkhalil805@gmail.com>
2026-09-06 17:52:53 +0100

    full securité hpp xss rateLimiting helemt

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,11 +1,11 @@
 const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,  /////////////////15 minutes
+  windowMs: 15 * 60 * 1000, /////////////////15 minutes
   max: 5,
   message: {
     success: false,
-    message: "Trop de tentatives. Réessayez plus tard.",
+    message: "many requirest please try again after 15 minutes",
   },
 });
 
--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,28 @@ app.use(helmet());
 //////////////////cors
 const cors = require("cors");
 app.use(cors());
+////////////////////rate limit
+
+// const rate = require("express-rate-limit");
+// app.use(
+//   rate({
+//     windowMs: 15 * 60 * 1000, /////////////////15 minutes
+//     max: 5,
+//     message: {
+//       success: false,
+//       message: "many requirest please try again after 15 minutes",
+//     },
+//   }),
+// );
 
 /////////////////////xss (cross site Scripting)
-const xss = require("xss-clean")
+const xss = require("xss-clean");
 app.use(xss());
+////////////////////hpp( http paramater pollution)
+const hpp = require("hpp");
+app.use(hpp());
 //////////////////////////connecter mongoose
-const connectDB= require("./config/db");
+const connectDB = require("./config/db");
 connectDB();
 
 const stagiareRouter = require("./routes/stagiareRoute");
@@ -30,4 +46,3 @@ const port = process.env.port;
 app.listen(port, () => {
   console.log("le serveur demare sur le port " + port);
 });
-
